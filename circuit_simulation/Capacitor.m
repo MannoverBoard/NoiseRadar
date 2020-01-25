e54ac19f1e7622155113e02df143f7b1c28b499a
Patrick <patrickdberry802@gmail.com>
2020-01-24 20:56:43 -0600

    Creating subclasses of Component for simple linear components RLC

---
+++ circuit_simulation/Capacitor.m
@@ -0,0 +1,60 @@
+classdef Capacitor < Component
+  properties (Constant)
+    type_name = 'Capacitor';
+    type_short_name = 'C';
+    Connectors = @()struct('pos',[],'neg',[]);
+    default_value = 1e-6;
+  end
+  properties(Dependent)
+    value;
+  end
+  methods
+    function self = Capacitor(varargin)
+      self@Component(varargin{:});
+      if nargin>=3
+        self.values = varargin{3};
+      else
+        self.values = [self.default_value];
+      end
+    end
+
+    function [value] = get.value(self)
+      value = self.values(1);
+    end
+    function set.value(self,value)
+      self.values(1) = value;
+    end
+  end
+  
+  methods (Abstract,Access=?CircuitEnvironment)
+    function [admittance_matrix] = addToCircuitAC(self,indexFromNode,admittance_matrix)
+      is_fully_connected = self.isFullyConnected();
+      if ~is_fully_connected
+        warning('Component:Unconnected','Component %s is not fully connected. Cannot add to circuit',self.name);
+        return;
+      elseif isempty(self.connectors)
+        %no connections
+        return;
+      end
+      connector_index = self.connectors;%copy
+      fns = fieldname(self.connectors);
+      for fn_idx = numel(fns)
+        fn = fns{fn_idx};
+        connector_index.(fn) = indexFromNode(self.connectors.(fn));
+      end
+      s = self.h_env.LaplaceVariable;
+      b = 1/(s*self.symbol(1));
+      pos = connector_index.pos;
+      neg = connector_index.neg;
+      admittance_matrix(pos,pos) =  b;
+      admittance_matrix(neg,neg) =  b;
+      admittance_matrix(pos,neg) = -b;
+      admittance_matrix(neg,pos) = -b;
+    end
+  end
+  methods(Abstract,Access=?Component)
+    function [self] = updateSymbols(self)
+      self.symbols = sym(sprinf('%s%d',self.type_short_name,self.uid));
+    end
+  end
+end
---
+++ circuit_simulation/Resistor.m
@@ -0,0 +1,59 @@
+classdef Resistor < Component
+  properties (Constant)
+    type_name = 'Resistor';
+    type_short_name = 'R';
+    Connectors = @()struct('pos',[],'neg',[]);
+    default_value = 1e3;
+  end
+  properties(Dependent)
+    value;
+  end
+  methods
+    function self = Resistor(varargin)
+      self@Component(varargin{:});
+      if nargin>=3
+        self.values = varargin{3};
+      else
+        self.values = [self.default_value];
+      end
+    end
+
+    function [value] = get.value(self)
+      value = self.values(1);
+    end
+    function set.value(self,value)
+      self.values(1) = value;
+    end
+  end
+  
+  methods (Abstract,Access=?CircuitEnvironment)
+    function [admittance_matrix] = addToCircuitAC(self,indexFromNode,admittance_matrix)
+      is_fully_connected = self.isFullyConnected();
+      if ~is_fully_connected
+        warning('Component:Unconnected','Component %s is not fully connected. Cannot add to circuit',self.name);
+        return;
+      elseif isempty(self.connectors)
+        %no connections
+        return;
+      end
+      connector_index = self.connectors;%copy
+      fns = fieldname(self.connectors);
+      for fn_idx = numel(fns)
+        fn = fns{fn_idx};
+        connector_index.(fn) = indexFromNode(self.connectors.(fn));
+      end
+      g = 1/self.symbol(1);
+      pos = connector_index.pos;
+      neg = connector_index.neg;
+      admittance_matrix(pos,pos) =  g;
+      admittance_matrix(neg,neg) =  g;
+      admittance_matrix(pos,neg) = -g;
+      admittance_matrix(neg,pos) = -g;
+    end
+  end
+  methods(Abstract,Access=?Component)
+    function [self] = updateSymbols(self)
+      self.symbols = sym(sprinf('%s%d',self.type_short_name,self.uid));
+    end
+  end
+end
