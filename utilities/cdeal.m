78109ff4fd5996272cc09b2345b8c4447803b611
Patrick <patrickdberry802@gmail.com>
2020-01-28 00:03:55 -0600

    Updating cdeal to handle non-matching number of arguments

--- utilities/cdeal.m
+++ utilities/cdeal.m
@@ -1,5 +1,9 @@
 function [varargout] = cdeal(X)
   %deals out a cell like it should
-  varargout = cell(size(X));
-  [varargout{:}]=X{:};
+  varargout = cell(1,nargout);
+  if nargin==0
+    return;
+  end
+  N = min([nargout,numel(X)]);
+  [varargout{1:N}]=X{1:N};
 end
